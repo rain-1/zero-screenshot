fee8ca1ba1d3ca5063c05f8324388486b589f92f
rain1 <rain1@airmail.cc>
2018-05-13 17:21:22 +0100

    Delete stuff: * Don't bother painting mouse cursor on. Drop Xfixes dep. * Don't convert to PNG. Drop pnglite dep. * Don't generate filename. * Don't bother with hotkeys/different screenshot modes * Don't do any command line arg parsing. * Don't print any text to stdout/stderr.

--- xscreenshot.c
+++ xscreenshot.c
@@ -5,11 +5,14 @@
  * License: GNU GPL Version 3 or later.
  *****/
 
+/*
+ * Modifications by Raymond Nicholson, 2018
+ */
+
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/cursorfont.h>
 #include <X11/Xregion.h>
-#include <X11/extensions/Xfixes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,13 +20,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <time.h>
-#include "pnglite.h"
-
-#define DEFAULT_SCREENSHOT_PREFIX "xscreenshot"
-#define DEFAULT_SCREENSHOT_TAKEN_MSG "Screenshot taken!"
-#define MSG_FONTNAME "dejavu sans"
-#define MSG_FONTSIZE "30"
-#define MSG_DELAY 3
 
 // so we don't pass thousands of parameters each time
 typedef struct
@@ -44,20 +40,6 @@ typedef struct
 
 	int screenshot_delay;
 } ProgState;
-
-
-// This must be freed when done
-char *get_save_fname(ProgState *st)
-{
-	char *fname;
-	char ts[64];
-	
-	time_t now = time(NULL);
-	struct tm *now2 = localtime(&now);
-	strftime(ts, sizeof(ts), "%Y%m%dT%H%M%S", now2);
-	asprintf (&fname, "%s-%s.png", st->screenshot_path, ts);
-	return fname;
-}
 
 ////////////// SCREEN CAPTURE ROUTINE //////////////
 // get shift count based on mask, assumes LSB
@@ -121,48 +103,10 @@ unsigned char *extract_rgb24_from_ximage (XImage *image)
 	return out;
 }
 
-// overlay the mouse cursor on top of given image.
-void paint_mouse_cursor(ProgState *st, BOX rect, unsigned char *image) {
-	/* Check if the XFIXES extension is present */
-	int event, error;
-	if (XFixesQueryExtension(st->display, &event, &error))
-	{
-		XFixesCursorImage *cur = XFixesGetCursorImage(st->display);
-		
-		int sx = (cur->x - cur->xhot), sy = (cur->y - cur->yhot);
-		int x,y,dx,dy, cx,cy;
-		for (y = sy, dy=cur->height, cy=0;  dy>0; dy--, y++, cy++) {
-			for (x = sx, dx=cur->width, cx=0; dx> 0; dx--, x++, cx++) {
-				
-				if (x>=rect.x1 && y>=rect.y1 && x<rect.x2 && y<rect.y2) {
-					unsigned long cur_pixel = cur->pixels [ cy*cur->width + cx ];
-					int r = (cur_pixel >> 0) & 0xff;
-					int g = (cur_pixel >> 8) & 0xff;
-					int b = (cur_pixel >> 16) & 0xff;
-					int a = (cur_pixel >> 24) & 0xff;
-					
-					int img_ofs = ((y-rect.y1)*rect.x2 + (x-rect.x1)) * 3; // 24-bit RGB - 3 bytes per pixel
-					if (a == 255) {
-						image [ img_ofs +0 ] = r;
-						image [ img_ofs +1 ] = g;
-						image [ img_ofs +2 ] = b;
-					} else {
-						image [ img_ofs +0 ] = r + (image [ img_ofs +0 ] * (255 - a) + 255 / 2) / 255;
-						image [ img_ofs +1 ] = g + (image [ img_ofs +1 ] * (255 - a) + 255 / 2) / 255;
-						image [ img_ofs +2 ] = b + (image [ img_ofs +2 ] * (255 - a) + 255 / 2) / 255;
-					}
-				}
-				
-			}
-		}
-	}
-}
-
-// take screenshot (if enabled), using pnglite
+// take screenshot (if enabled)
 void take_screenshot (ProgState *st, BOX rect, int with_mouse) // x2,y2 is width, height
 {
 	XImage *image;
-	png_t png;
 	unsigned char *buf;
 	char *fname=NULL;
 	
@@ -176,59 +120,12 @@ void take_screenshot (ProgState *st, BOX rect, int with_mouse) // x2,y2 is width
 		AllPlanes, ZPixmap
 	);
 	buf = extract_rgb24_from_ximage (image);
-	if (with_mouse) 
-		paint_mouse_cursor (st, rect, buf);
+
+	// TODO emit screenshit
 	
-	// save it
-	if (buf) {
-		fname = get_save_fname(st);
-		png_open_file_write (&png, fname);
-		png_set_data (&png, image->width, image->height, 8, PNG_TRUECOLOR, buf); //8 bits per channel.
-		png_close_file (&png);
-		free (buf);	
-		free (fname);
-	}
 	XDestroyImage (image);
-	fprintf (stderr, "screenshot taken\n");
 }
 ///////////////// END OF SCREEN CAPTURE ROUTINE ///////////////
-
-
-// enable notification when our hotkey is pressed
-char *grab_hotkey (ProgState *st, char *hotkey)
-{
-	char *hotkey_name = hotkey;
-	unsigned int keycode = 0, keycode2 = 0;
-	KeySym keysym, keysym2;
-	
-	keysym = XStringToKeysym (hotkey);
-	if (keysym != NoSymbol)
-		keycode = XKeysymToKeycode (st->display, keysym);
-	else {
-		keycode = atoi(hotkey);
-		if (keycode) {
-			keysym2 = XKeycodeToKeysym(st->display, keycode, 0);
-			keycode2 = XKeysymToKeycode (st->display, keysym2);
-			hotkey_name = XKeysymToString (keysym2);
-		}
-	}
-		
-	if (keycode)
-		XGrabKey (st->display, keycode, AnyModifier, st->root_window, 
-		          False, GrabModeAsync, GrabModeAsync);
-	if (keycode2 && keycode2 != keycode)
-		XGrabKey (st->display, keycode2, AnyModifier, st->root_window, 
-		          False, GrabModeAsync, GrabModeAsync);	
-	if (!keycode && !keycode2) 
-	{
-		fprintf (stderr, "I don't understand %s, bailing out.\n", hotkey);
-		exit(1);
-	}
-	st->hotkey_keycode = keycode;
-	st->hotkey_keycode2 = keycode2;
-	return hotkey_name;
-}
-
 
 // grap or ungrab the mouse pointer
 int grab_ungrab_mouse (ProgState *st, int grab)
@@ -249,7 +146,7 @@ int grab_ungrab_mouse (ProgState *st, int grab)
 
 
 // draw a rectangle, x2,y2 is width, height
-draw_rect (ProgState *st, BOX rect)
+void draw_rect (ProgState *st, BOX rect)
 {
 	//fprintf (stderr, "%d, %d, %d, %d\n", box->x1, box->y1, width, height);
 	XDrawRectangle (
@@ -320,18 +217,6 @@ void notify(ProgState *st, char *msg, int x, int y, int delay) {
 	sleep (delay);
 	XDrawString(st->display, st->root_window, st->pen, x, y, msg, msglen);
 	XSync(st->display, False);	
-}
-
-// perform delayed screen capture
-void delayed_capture(ProgState *st, int with_mouse)
-{
-	BOX rect = { .x1=0, .y1=0, .x2=st->width, .y2=st->height };
-
-	fprintf (stderr, "screenshot in %d seconds\n", st->screenshot_delay);
-	sleep (st->screenshot_delay);
-	take_screenshot (st, rect, with_mouse);
-	fprintf (stderr, "screenshot taken\n");
-	notify (st, "screenshot taken\n", 0, atoi(MSG_FONTSIZE), MSG_DELAY);
 }
 
 // get the area of window under cursor
@@ -465,14 +350,6 @@ next_event:
 							}
 							break;
 							
-						case 1: // shift - delayed fullscreen shot with mouse
-							delayed_capture (st, 1);
-							break;
-							
-						case 2: // ctrl - delayed fullscreen shot without mouse
-							delayed_capture (st, 0);
-							break;							
-							
 						case 3: // ctrl-shift - exit
 							if (st->select_in_progress) 
 								end_selection (st);
@@ -487,103 +364,33 @@ next_event:
 	return 0;
 }
 
-
-// ***************** Entry point *************
-int main (int argc, char*argv[])
-{
+int main (int argc, char *argv[]) {
 	ProgState st;
-	char *hotkey = NULL;
 	XGCValues values;
-	unsigned long gcmask;
-	XFontStruct *font;
-	int opt;
 
-	// default values
-	st.hotkey_keycode = st.hotkey_keycode2 = 0;
-	asprintf (&st.screenshot_path, "%s/" DEFAULT_SCREENSHOT_PREFIX, getenv("HOME"));
-	st.screenshot_delay = 5; // default delay is 5 seconds
-	
-	// argument processing	
-	while ((opt = getopt(argc, argv, "n:p:k:d:h")) != -1) 
-	{
-		switch (opt) 
-		{
-			case 'k':
-				if (optarg) hotkey = optarg;
-				break;
-			case 'p':
-				if (optarg) st.screenshot_path = optarg;
-				break;
-			case 'd':
-				st.screenshot_delay = atoi (optarg);
-				break;
-			case 'h':
-				fprintf (stderr,
-"Usage: xscreenshot [-k hotkey] [-p path] [-n index]\n"
-"Copyright (C) James Budiono, 2014. License: GPL version 3.0 or later\n\n"
-" -h      : this text\n"
-" -k key  : multiple shots mode, use 'key' as hotkey (default none)\n"
-" -p path : path/filename for screenshot (default is $HOME/" DEFAULT_SCREENSHOT_PREFIX ")\n"
-" -d delay: the delay (in seconds) for fullscreen screenshot (default 5)\n\n"
-"If you don't use -k, the program will take one screenshot and exit.\n"
-"If you do: press hotkey to enter screenshot taking mode, take as many\n"
-"  screenshots, and press hotkey to return back to normal mode.\n"
-"  Press Ctrl-Shift-hotkey to terminate program\n"
-);
-				return 0;
-		}
+	st.display = XOpenDisplay(NULL);
+	if (!st.display) {
+		return 1;
 	}
-
-	fprintf (stderr,
-"--- Mouse usage ---\n"
-"Left-click  - press to start, move to select, release to take shot\n"
-"Right-click - cancel current selection, (in single-shot: once more to exit.)\n");
-
-	// initialisations
-	png_init (0,0);
-    st.display = XOpenDisplay(NULL);
-    if (st.display == NULL) {
-        fprintf(stderr, "Cannot open display\n");
-        exit(1);
-    }
-    st.screen      = DefaultScreen (st.display);
-    st.root_window = DefaultRootWindow (st.display);
-    st.width       = DisplayWidth(st.display, st.screen);
-    st.height      = DisplayHeight(st.display, st.screen);
-    st.grab_cursor = XCreateFontCursor (st.display, XC_crosshair);
+	st.screen      = DefaultScreen (st.display);
+	st.root_window = DefaultRootWindow (st.display);
+	st.width       = DisplayWidth(st.display, st.screen);
+	st.height      = DisplayHeight(st.display, st.screen);
+	st.grab_cursor = XCreateFontCursor (st.display, XC_crosshair);
 
 	values.function = GXinvert;
 	values.subwindow_mode = IncludeInferiors;
 	values.line_width = 1;
-	gcmask = GCFunction | GCSubwindowMode | GCLineWidth;
-	font = XLoadQueryFont(st.display, "-*-" MSG_FONTNAME "-*-*-*--" MSG_FONTSIZE "-*-*-*-*-*-*-*");
-	if (font)
-	{
-		values.font = font->fid;
-		gcmask |= GCFont;
-	}
-    st.pen = XCreateGC (st.display, st.root_window, gcmask, &values);
-    
-    if (hotkey) {
-		char *hotkey_name = grab_hotkey (&st, hotkey);
-		fprintf (stderr,
-"--- Multiple shots mode enabled ---\n"
-"%s            - to toggle screenshot taking mode\n"
-"Shift-%s      - fullscreen delayed screenshot with mouse\n"
-"Ctrl-%s       - fullscreen delayed screenshot without mouse\n"
-"Ctrl-Shift-%s - exit\n", hotkey_name, hotkey_name, hotkey_name, hotkey_name);
-	}
-    else grab_ungrab_mouse (&st, 1);
-    fprintf(stderr, "---\n");
-
-	// run
+	st.pen = XCreateGC (st.display, st.root_window, GCFunction | GCSubwindowMode | GCLineWidth, &values);
+	
+	grab_ungrab_mouse (&st, 1);
 	st.select_in_progress = 0;
-    event_loop (&st);
-    
-    // cleanup
-    XFreeGC (st.display, st.pen);
+	event_loop (&st);
+
+	XFreeGC (st.display, st.pen);
 	XFlush (st.display);
 	XCloseDisplay (st.display);
+	
 	return 0;
 }
 
